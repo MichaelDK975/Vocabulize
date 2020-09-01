4cb8791df6d90eb29d4a380a9d09a22250dc0b34
MichaelDK975 <59149035+MichaelDK975@users.noreply.github.com>
2020-08-31 23:36:19 -0400

    Now the program writes each definition in seperate buttons and has custom textbox.

--- ChromeExtension/getDefinitions.js
+++ ChromeExtension/getDefinitions.js
@@ -11,38 +11,59 @@ $(window).on('load', function() {
       //  })  
     //text.innerHTML = "herllo";
     //var text = document.getElementById('text');
-    
-    let chosenWord = "";
-    chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
-    function(tab) {
-      chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
-      function(response){
-        //var text = document.getElementById('text'); 
-        text.innerHTML = response.data;
-        //myAwesomeFancyFunction(response.data);
-      });
-    });
-    
-    chosenWord = "virtuoso";
+
     //chosenWord = text.innerHTML;
     //text.innerHTML = chosenWord;
-
-    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${chosenWord}?key=fa5c828f-849a-4617-a2f7-654b10474848`)
-        .then(response => response.json())
-        .then(json => {
-            // After the API is called and data is returned, the following code is executed:
-            let myDiv = document.getElementById("message");
-            //myDiv.innerHTML = JSON.stringify(json);
-            myDiv.innerHTML = json[0].shortdef;        
-        })   
-
+    doSomethingWithSelection(addsToDefinition);
+    //doSomethingWithSelection(reWriteSelectedWord);
 
     
 });
 
+function reWriteSelectedWord(selectedWord){
+    let whereToPutWord = document.getElementById("theWord");
+    whereToPutWord.innerHTML = selectedWord;
+}
 
-function MyAwesomeFancyFunction(word) {
+// Combines the dictionary API lookup with putting the output on the screen into a DIV
+function addsToDefinition(selectedWord){
+    reWriteSelectedWord(selectedWord);
+    getTheDefinitionAndDoSomethingWithIt(selectedWord,putJSONIntoADiv);
+}
+
+// Takes the JSON previously returned, and puts it on the screen - replacing a DIV, in button format
+function putJSONIntoADiv(json) {
+        // After the API is called and data is returned, the following code is executed:
     let myDiv = document.getElementById("message");
-    myDiv.innerHTML = JSON.stringify(json);
-    myDiv.innerHTML = word;
-};
+    //myDiv.innerHTML = JSON.stringify(json);
+
+    let allTheDefinitions = "";
+    let definitionCounter = 1;
+    for (eachSingleThing of json) {
+        if(eachSingleThing.shortdef!=""){
+            allTheDefinitions = allTheDefinitions + "<button style='text-align:left;'>"+definitionCounter +". " + eachSingleThing.shortdef +"</button><br/>" ;
+            definitionCounter +=1;
+        }
+    }
+    allTheDefinitions += '<div><textarea placeholder = "Custom Definition"></textarea></div>';
+    allTheDefinitions += "<button>Add Custom Definition</button>"
+    myDiv.innerHTML = allTheDefinitions;        
+}
+
+
+
+// Calls the dictionary API with any given word, and then calls our function with the JSON result of that API call 
+function getTheDefinitionAndDoSomethingWithIt(selectedWord, whatToDoWithTheJSON){
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${selectedWord}?key=fa5c828f-849a-4617-a2f7-654b10474848`)
+        .then(response => response.json())
+        .then(json => {
+            whatToDoWithTheJSON(json);
+        })
+}
+
+
+// function MyAwesomeFancyFunction(word) {
+//     let myDiv = document.getElementById("message");
+//     myDiv.innerHTML = JSON.stringify(json);
+//     myDiv.innerHTML = word;
+// };
--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -6,14 +6,19 @@ $(function(){
   function pasteSelection() {
     var text = document.getElementById('text');
     //text.innerHTML = "hello";
+    doSomethingWithSelection( (rd) => {
+          var text = document.getElementById('text'); 
+          text.innerHTML = rd;
+    });
+  }
 
+  // Use send-message functionality to retrieve the selected text, and then call our own function with that text 
+  function doSomethingWithSelection(myOwnFunction) {
     chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
-    function(tab) {
-      chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
-      function(response){
-        var text = document.getElementById('text'); 
-        text.innerHTML = response.data;
-        //myAwesomeFancyFunction(response.data);
-      });
+      function(tab) {
+        chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
+        function(response){
+          myOwnFunction(response.data);
+        });
     });
   }
